fe7c6e34d809e4d7069585518d4d69dacbf5b1b6
Nelsonfrank <Nelsonfr78@outlook.com>
2019-05-31 13:15:28 +0300

    Feat: Create Profile Component to display user information  #14

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import FindRide from "./Components/FindRide";
 import App from "./App";
+import Profile from "./Components/Profile";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Profile />, document.getElementById("root"));
