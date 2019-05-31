6b52e9042b8e739152c4b992586975695727b803
Nelsonfrank <Nelsonfr78@outlook.com>
2019-05-31 20:00:44 +0300

    Feat: Modify index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import FindRide from "./Components/FindRide";
 import App from "./App";
-import Profile from "./Components/Profile";
 import "./index.css";
 
-ReactDOM.render(<Profile />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
