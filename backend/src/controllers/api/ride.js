a2e13aedc39af1230fda8848108ce239234d0f98
Nelsonfrank <Nelsonfr78@outlook.com>
2019-06-01 15:53:21 +0300

    refactor(ride): POST /ride
    Refactoring create ride api

---
+++ backend/src/controllers/api/ride.js
@@ -0,0 +1,30 @@
+export async function getAllRides(request, response) {
+  try {
+    const rides = [];
+
+    return response.status(200).json(rides);
+  } catch (error) {
+    const { message } = error;
+
+    return response.status(500).json({
+      error,
+      message
+    });
+  }
+}
+
+export async function createRide(request, response) {
+  try {
+    const rides = [];
+
+    rides.concat(request.body);
+    return response.status(201).json(rides);
+  } catch (error) {
+    const { message } = error;
+
+    return response.status(500).json({
+      error,
+      message
+    });
+  }
+}
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,76 +1,60 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import auth from '../auth'; // eslint-disable-line no-unused-vars
-import * as authController  from '../controllers/api/auth';
-import * as userController from '../controllers/api/user';
+import auth from "../auth"; // eslint-disable-line no-unused-vars
+import * as authController from "../controllers/api/auth";
+import * as rideController from "../controllers/api/ride";
+import * as userController from "../controllers/api/user";
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const message = {
-    name: 'express-starter',
-    license: 'MIT',
+    name: "express-starter",
+    license: "MIT",
     keywords: [
-      'express',
+      "express",
       "babel",
-      'boilerplate',
-      'scaffold',
-      'es6',
+      "boilerplate",
+      "scaffold",
+      "es6",
       "es2015",
-      'es2016',
-      'es2017',
-      'jest',
-      'eslint'
+      "es2016",
+      "es2017",
+      "jest",
+      "eslint"
     ],
     engines: {
-      "node": "~6.9.1",
-      'npm': ">=3.10.0"
-    },
+      node: "~6.9.1",
+      npm: ">=3.10.0"
+    }
   };
 
-  return res
-    .status(200)
-    .json(message);
+  return res.status(200).json(message);
 });
 
 /**
  * GET /rides Fetch all rides.
  */
-router.get('/rides', (req, res) => {
-  const rides = [];
-
-  return res
-    .status(200)
-    .json(rides);
-});
+router.get("/rides", rideController.getAllRides);
 
 /**
  * POST /rides Create a ride offer.
  */
-router.post('/rides', (req, res) => {
-  const rides = [];
-
-  rides.concat(req.body);
-
-  return res
-    .status(201)
-    .json(rides);
-});
+router.post("/rides", rideController.createRide);
 
 /**
  * POST /register Register a user
  */
-router.post('/register', authController.register);
-
+router.post("/register", authController.register);
 
 /**
  * POST /login Log in
  */
-router.post('/login', authController.login);
+router.post("/login", authController.login);
 
 /**
  * GET /user Get authenticated user
  */
-router.get('/user', /*[auth, ],*/ userController.getAuthUser);
+router.get("/user", /*[auth, ],*/ userController.getAuthUser);
 
 export default router;
